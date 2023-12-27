42a892b265f62c15fc231662362ee403092a4706
Harkirat Singh <harkirat96@gmail.com>
2023-12-27 22:49:23 +0530

    checkpoint

---
+++ backend/src/managers/IoManager.ts
@@ -0,0 +1,17 @@
+import http from 'http';
+
+import { Server } from "socket.io";
+const server = http.createServer();
+
+
+export class IoManager {
+    private static io: Server;
+
+    public static getIo() {
+        if (!this.io) {
+            const io = new Server(server);
+            this.io = io;
+        }
+        return this.io;
+    }
+}
