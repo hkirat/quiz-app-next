43b732676d0430eba0791b1f4f6d3b3a2b942537
Aarav <aaravkhokhar@yahoo.in>
2023-12-27 23:50:50 +0530

    fix names and images

--- frontend/src/screens/Leaderboard.tsx
+++ frontend/src/screens/Leaderboard.tsx
@@ -1,15 +1,25 @@
 import { ArrowLeft, ArrowRight, ThumbsUp } from "lucide-react";
 
 const data = [
-  { val: 1000, name: "Ambriel" },
-  { val: 975, name: "Pavan Sunke" },
-  { val: 971, name: "Namish Kapoor" },
-  { val: 960, name: "Ambriel" },
-  { val: 925, name: "Pavan Sunke" },
-  { val: 900, name: "Namish Kapoor" },
-  { val: 870, name: "Ambriel" },
-  { val: 850, name: "Pavan Sunke" },
-  { val: 830, name: "Namish Kapoor" },
+  {
+    points: 1000,
+    username: "Ambriel",
+    profilePicture:
+      "https://plus.unsplash.com/premium_photo-1703385178754-f16e0a332e4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  { points: 975, username: "Pavan Sunke" },
+  { points: 971, username: "Namish Kapoor" },
+  {
+    points: 960,
+    username: "Ambriel",
+    profilePicture:
+      "https://plus.unsplash.com/premium_photo-1703385178754-f16e0a332e4a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  { points: 925, username: "Pavan Sunke" },
+  { points: 900, username: "Namish Kapoor" },
+  { points: 870, username: "Ambriel" },
+  { points: 850, username: "Pavan Sunke" },
+  { points: 830, username: "Namish Kapoor" },
 ];
 
 function getRandomRgbColor(): string {
@@ -33,24 +43,41 @@ function Leaderboard() {
       <h1 className="my-8 text-8xl font-semibold">Leaderboard</h1>
       <div className="mx-auto w-max">
         {data
-          .sort((a, b) => b.val - a.val)
+          .sort((a, b) => b.points - a.points)
           .slice(0, 10)
-          .map((item) => (
-            <div className="flex mb-2 gap-4 items-center">
-              <h3 className="font-semibold text-2xl w-[100px] text-right">
-                {item.val} p
-              </h3>
-              <div
-                className="animate-slide"
-                style={{
-                  height: "3rem",
-                  width: item.val * 0.75,
-                  backgroundColor: getRandomRgbColor(),
-                }}
-              ></div>
-              <h3 className="text-2xl">{item.name}</h3>
-            </div>
-          ))}
+          .map((item) => {
+            const color = getRandomRgbColor();
+            return (
+              <div className="flex mb-2 gap-4 items-center">
+                <h3 className="font-semibold text-2xl w-[100px] text-right">
+                  {item.points} p
+                </h3>
+                <div
+                  className="animate-slide relative mr-4"
+                  style={{
+                    height: "3rem",
+                    width: item.points * 0.75,
+                    backgroundColor: color,
+                  }}
+                >
+                  <div
+                    className="absolute right-[-20px]  border-white border-2 h-12 w-12 rounded-full grid place-content-center overflow-hidden"
+                    style={{ backgroundColor: color }}
+                  >
+                    {item.profilePicture ? (
+                      <img
+                        src={item.profilePicture}
+                        alt="Profile picture"
+                        className="h-10 w-10 object-cover rounded-full"
+                      />
+                    ) : null}
+                  </div>
+                </div>
+
+                <h3 className="text-2xl">{item.username}</h3>
+              </div>
+            );
+          })}
       </div>
       <div className="w-full absolute bottom-0 flex justify-between">
         <div>
