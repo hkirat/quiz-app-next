a182504c2db5b214e465410b874bbacaa4df0170
hustlerZzZ <rishabpal2000@gmail.com>
2023-12-27 23:17:44 +0530

    minor fix

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -6,7 +6,7 @@ function Leaderboard() {
 
     return (
         <div className="bg-opacity-20 bg-white backdrop-blur-5 border border-opacity-30 border-solid border-white dark:border-gray-800 dark:bg-gray-800 dark:bg-opacity-20 p-6 rounded-lg shadow-md">
-            <h1 className="text-2xl text-center my-4 text-wh">
+            <h1 className="text-2xl text-center my-4 text-white">
                 Leaderboard Results 🚀
             </h1>
             <div className="overflow-y-scroll h-96">
