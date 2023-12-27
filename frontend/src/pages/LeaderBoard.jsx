23bf52240653cd3f3afa189c3c56912decf1d40b
Harsh Deep <hdeep61034@gmail.com>
2023-12-27 23:27:31 +0530

    UI Improvement in LeaderBoard

--- frontend/src/pages/LeaderBoard.jsx
+++ frontend/src/pages/LeaderBoard.jsx
@@ -24,23 +24,31 @@ const LeaderBoard = () => {
 
   return (
     <>
-      <h1 className="text-4xl my-4 text-bold text-center">Leaderboard</h1>
+      <h1 className="text-5xl m-4 text-center font-bold">Leaderboard</h1>
       <div className="leaderboard-container w-screen h-screen flex justify-center items-center">
         <table className="border-black border-2 text-3xl">
           <thead>
             <tr>
-              <th className="border-2 border-black p-2">Rank</th>
-              <th className="border-2 border-black p-2">Username</th>
-              <th className="border-2 border-black p-2">Profile Picture</th>
-              <th className="border-2 border-black p-2">Points</th>
+              <th className="border-2 border-black p-2 bg-blue-500">Rank</th>
+              <th className="border-2 border-black p-2  bg-blue-500">
+                Username
+              </th>
+              <th className="border-2 border-black p-2  bg-blue-500">
+                Profile Picture
+              </th>
+              <th className="border-2 border-black p-2  bg-blue-500">Points</th>
             </tr>
           </thead>
           <tbody>
             {sortedWinners.map((winner, index) => (
               <tr key={index}>
-                <td className="border-2 border-black p-2">{index + 1}</td>
-                <td className="border-black border-2 p-2">{winner.username}</td>
-                <td className="border-2 border-black p-2">
+                <td className="border-2 border-black p-2 bg-blue-400">
+                  {index + 1}
+                </td>
+                <td className="border-black border-2 p-2 bg-green-300">
+                  {winner.username}
+                </td>
+                <td className="border-2 border-black p-2 bg-red-400">
                   {winner.profilePicture && (
                     <img
                       src={winner.profilePicture}
@@ -48,7 +56,9 @@ const LeaderBoard = () => {
                     />
                   )}
                 </td>
-                <td className="border-black border-2 p-2">{winner.points}</td>
+                <td className="border-black border-2 p-2 bg-yellow-200">
+                  {winner.points}
+                </td>
               </tr>
             ))}
           </tbody>
