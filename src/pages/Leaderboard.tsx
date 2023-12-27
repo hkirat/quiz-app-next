b7416083fe8f548c86474bf9cf5293b6a625c2dd
hustlerZzZ <rishabpal2000@gmail.com>
2023-12-27 23:08:38 +0530

    Added Rocket Emoji

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -6,7 +6,9 @@ function Leaderboard() {
 
     return (
         <div className="bg-black text-white p-8 rounded-md">
-            <h1 className="text-2xl text-center my-4">Leaderboard Results</h1>
+            <h1 className="text-2xl text-center my-4">
+                Leaderboard Results 🚀
+            </h1>
             <div className="overflow-y-scroll h-96">
                 {leaderboardData.map((el, index) => (
                     <Card
