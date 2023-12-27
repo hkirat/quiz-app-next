76693aa0475c6618f25324f294d9221d63b472f7
devsargam <sargampoudel100@gmail.com>
2023-12-28 00:04:57 +0530

    feat: add simple quiz system and mock data

---
+++ client/src/mocks/quiz-mock.ts
@@ -0,0 +1,21 @@
+export const quizData = [
+  {
+    title: 'What is the capital of France?',
+    choices: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+    imageURL: 'https://example.com/france.jpg',
+  },
+  {
+    title: 'Which planet is known as the Red Planet?',
+    choices: ['Earth', 'Mars', 'Venus', 'Jupiter'],
+    imageURL: 'https://example.com/mars.jpg',
+  },
+  {
+    title: 'What is the largest mammal?',
+    choices: ['Elephant', 'Blue Whale', 'Giraffe', 'Lion'],
+  },
+  {
+    title: 'In which year did the Titanic sink?',
+    choices: ['1905', '1912', '1920', '1935'],
+    imageURL: 'https://example.com/titanic.jpg',
+  },
+];
