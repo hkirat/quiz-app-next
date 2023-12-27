310940814aeca077089b910ace5c5f9f22516625
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-28 00:39:00 +0530

    📦 NEW: add footer

--- frontend-components/src/app/layout.tsx
+++ frontend-components/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
