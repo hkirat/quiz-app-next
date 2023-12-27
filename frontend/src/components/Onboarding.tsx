2e3a0003c94f3a07892cb77ef741a6de6784813e
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-28 00:17:49 +0530

    🐛 FIX: add link to quiz

--- frontend/src/components/Onboarding.tsx
+++ frontend/src/components/Onboarding.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Onboarding() {
   return (
     <>
@@ -28,12 +30,14 @@ export default function Onboarding() {
             </div>
 
             <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
-              >
-                Join
-              </button>
+              <Link href="/quiz">
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+                >
+                  Join
+                </button>
+              </Link>
             </div>
           </form>
 
