b0bf4a8ac825db9032e55053605e394fd6176a28
iamsidar07 <ms8460149@gmail.com>
2024-01-01 09:01:56 +0530

    Implemented join room , leader board,  and question screen UI

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -5,125 +5,141 @@ import { LeaderBoard } from "./leaderboard/LeaderBoard";
 import { Quiz } from "./Quiz";
 
 export const User = () => {
-    const [name, setName] = useState("");
-    const [submitted, setSubmitted] = useState(false);
-    const [code, setCode] = useState("");
-    if (!submitted) {
-        return <div>
-            <div className="bg-gray-100 flex items-center justify-center h-screen">
-                <div className="text-center">
-                <div className="mb-8">
-                    <h1 className="text-2xl font-semibold mb-2 text-slate-600">
-                        Enter the code to join
-                    </h1>
-                    <p className="text-gray-600">It’s on the screen in front of you</p>
-                </div>
-                <div className="mb-8">
-                    <input
-                        className="text-center w-64 p-2 border-2 border-purple-600 rounded-lg shadow-sm focus:outline-none focus:border-purple-800"
-                        placeholder="1234 5678"
-                        style={{ fontSize: "1rem" }}
-                        type="text"
-                        onChange={(e) => {
-                            setCode(e.target.value)
-                        }}
-                    />
-                    <br /> <br />
-                    <input
-                        className="text-center w-64 p-2 border-2 border-purple-600 rounded-lg shadow-sm focus:outline-none focus:border-purple-800"
-                        placeholder="Your name"
-                        style={{ fontSize: "1rem" }}
-                        type="text"
-                        onChange={(e) => {
-                            setName(e.target.value)
-                        }}
-                    />
-                </div>
-                <button
-                    className="bg-purple-600 text-white w-64 py-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-800 focus:ring-opacity-50"
-                    style={{ fontSize: "1rem" }}
-                    onClick={() => {
-                        setSubmitted(true);
-                    }}
-                >
-                    Join
-                </button>
-                </div>
+  const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [code, setCode] = useState("");
+  if (!submitted) {
+    return (
+      <div>
+        <div className="bg-[#FFFFFF] flex items-center justify-center h-screen">
+          <div className="text-center">
+            <div className="mb-8">
+              <h1 className="text-2xl font-semibold mb-2 text-slate-600">
+                Enter the code to join
+              </h1>
+              <p className="text-gray-600">
+                It’s on the screen in front of you
+              </p>
             </div>
-
+            <div className="mb-8">
+              <input
+                className="w-80 p-4 rounded-xl bg-[#F4F4F6] text-left text-black outline-offset-2 outline-4 focus:outline outline-[#E8E9FF] focus:border focus:border-purple-500 hover:border "
+                placeholder="1234 5678"
+                style={{ fontSize: "1rem" }}
+                type="text"
+                onChange={(e) => {
+                  setCode(e.target.value);
+                }}
+              />
+              <br />
+              <input
+                className="w-80 mt-4 p-4 rounded-xl bg-[#F4F4F6] text-left text-black outline-offset-2 outline-4 focus:outline outline-[#E8E9FF] focus:border focus:border-purple-500 hover:border "
+                placeholder="Your name"
+                style={{ fontSize: "1rem" }}
+                type="text"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+            <button
+              className="bg-[#2C2C2D] text-white w-64 px-8 py-4 rounded-full shadow-md hover:bg-[#2c2c2df1] focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+              style={{ fontSize: "1rem" }}
+              onClick={() => {
+                setSubmitted(true);
+              }}
+            >
+              Join
+            </button>
+          </div>
         </div>
-    }
+      </div>
+    );
+  }
 
-    return <UserLoggedin code={code} name={name} />
-}
+  return <UserLoggedin code={code} name={name} />;
+};
 
-export const UserLoggedin = ({name, code}) => {
-    const [socket, setSocket] = useState<null | any>(null);
-    const roomId = code;
-    const [currentState, setCurrentState] = useState("not_started");
-    const [currentQuestion, setCurrentQuestion] = useState<any>(null);
-    const [leaderboard, setLeaderboard] = useState([]);
-    const [userId, setUserId] = useState("");
+export const UserLoggedin = ({ name, code }) => {
+  const [socket, setSocket] = useState<null | any>(null);
+  const roomId = code;
+  const [currentState, setCurrentState] = useState("not_started");
+  const [currentQuestion, setCurrentQuestion] = useState<any>(null);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [userId, setUserId] = useState("");
 
-    useEffect(() => {
-        const socket = io("https://sum-server.100xdevs.com");
-        setSocket(socket)
+  useEffect(() => {
+    const socket = io("https://sum-server.100xdevs.com");
+    setSocket(socket);
 
-        socket.on("connect", () => {
-            console.log(socket.id);
-            socket.emit("join", {
-                roomId,
-                name
-            })
-        });
-        
-        socket.on("init", ({userId, state}) => {
-            setUserId(userId);
+    socket.on("connect", () => {
+      console.log(socket.id);
+      socket.emit("join", {
+        roomId,
+        name,
+      });
+    });
 
-            if (state.leaderboard) {
-                setLeaderboard(state.leaderboard)
-            }
+    socket.on("init", ({ userId, state }) => {
+      setUserId(userId);
 
-            if (state.problem) {
-                setCurrentQuestion(state.problem);
-            }
+      if (state.leaderboard) {
+        setLeaderboard(state.leaderboard);
+      }
 
-            setCurrentState(state.type);
-        });
+      if (state.problem) {
+        setCurrentQuestion(state.problem);
+      }
 
-        socket.on("leaderboard", (data) => {
-            setCurrentState("leaderboard");
-            setLeaderboard(data.leaderboard);
-        });
-        socket.on("problem", (data) => {
-            setCurrentState("question");
-            setCurrentQuestion(data.problem);
-        })
-    }, []);
+      setCurrentState(state.type);
+    });
 
-    if (currentState === "not_started") {
-        return <div>
-            This quiz hasnt started yet
-        </div>
-    }
-    if (currentState === "question") {
-        return <Quiz roomId={roomId} userId={userId} problemId={currentQuestion.id} quizData={{
-            title: currentQuestion.description,
-            options: currentQuestion.options
-        }} socket={socket} />
-    }
+    socket.on("leaderboard", (data) => {
+      setCurrentState("leaderboard");
+      setLeaderboard(data.leaderboard);
+    });
+    socket.on("problem", (data) => {
+      setCurrentState("question");
+      setCurrentQuestion(data.problem);
+    });
+  }, []);
 
-    if (currentState === "leaderboard") {
-        return <LeaderBoard leaderboardData={leaderboard.map((x: any) => ({
-            points: x.points,
-            username: x.name,
-            image: x.image
-        }))} />
-    }
+  if (currentState === "not_started") {
+    return <div>This quiz hasnt started yet</div>;
+  }
+  if (currentState === "question") {
+    return (
+      <Quiz
+        roomId={roomId}
+        userId={userId}
+        problemId={currentQuestion.id}
+        quizData={{
+          title: currentQuestion.description,
+          options: currentQuestion.options,
+        }}
+        socket={socket}
+      />
+    );
+  }
 
-    return <div>
-        <br/>
-        Quiz has ended
-        {currentState}
+  if (currentState === "leaderboard") {
+    return (
+      <LeaderBoard
+        leaderboardData={leaderboard.map((x: any) => ({
+          points: x.points,
+          username: x.name,
+          image: x.image,
+        }))}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <br />
+      Quiz has ended
+      {currentState}
     </div>
-}
+  );
+};
+
